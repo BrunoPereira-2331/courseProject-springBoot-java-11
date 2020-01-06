d18ee4662c64e8a644f178b60b84044220751237
Bruno Pereira <Brunopereira.19@hotmail.com>
2020-01-06 13:20:25 -0300

    Project created

---
+++ src/test/java/com/example/Project/web/services/ProjectWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Project.web.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
